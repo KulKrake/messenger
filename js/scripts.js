bc767d13f29e2993ceaebdcac3fb210393af56be
KulKrake <gvanca05.vax@gmail.com>
2022-06-10 16:51:52 +0400

    project #5

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+function gamochena(){
+    document.getElementById("chatcontainer").style.display = "block";
+}
+function chaweva(){
+    document.getElementById("chatcontainer").style.display = "none";
+}
